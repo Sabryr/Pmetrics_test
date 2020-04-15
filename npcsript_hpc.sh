60ac36732c87d754f1fc36e54083772433fb1c7e
Sabry Razick <sabryr@gmail.com>
2020-04-15 23:09:05 +0200

    For public repository

---
+++ npcsript_hpc.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+echo $@
+cd "$1"
+ls -lhrt 
+PM_RLIB="$2"
+REPORTSCRIPT="$3"
+echo Linux>time.txt
+date +%s>>time.txt
+./np_prep MacOSX < PMcontrol
+echo 1 > extnum
+echo go > go
+gfortran -mcmodel=medium -m64 -w -O3 -o np_run $PM_RLIB/Pmetrics/compiledFortran/sNPeng.o npagdriv.f
+./np_run < go
+echo;echo Cleaning up....;echo
+stty sane
+mkdir inputs 
+mkdir outputs 
+mkdir wrkcopy  
+mkdir etc  
+echo data.csv >> NP_RF0001.TXT
+if [ ! -f NP_RF0001.TXT ]; then error=true; else error=false; fi
+if [ -f DEN* ]; then mv DEN* outputs; fi
+if [ -f OUT0* ]; then mv OUT0* outputs; fi
+if [ -f OUTT* ]; then mv OUTT* outputs; fi
+if [ -f PRTB* ]; then mv PRTB* outputs; fi
+if [ -f ILOG* ]; then mv ILOG* outputs; fi
+if [ -f NP_RF* ]; then mv NP_RF* outputs; fi
+if [ -f ERROR* ]; then mv ERROR* outputs; fi
+mv instr.inx etc
+mv log.txt outputs
+mv PMcontrol etc
+mv model.for etc/model.for
+mv model.txt inputs/model.txt
+mv XQZPJ*.ZMQ wrkcopy
+mv extnum etc
+mv npag*.* etc
+rm CHMAX*.*
+if [ -f FROM0001 ]; then mv FROM0001 inputs; fi
+rm fort.*
+rm go
+mv np_prep* etc
+mv np_run* etc
+mv data.csv inputs
+date +%s >> time.txt
+mv time.txt outputs
+if ! $error ; then 
+echo Rscript $REPORTSCRIPT $1'/outputs' 'median' 'NPAG' TRUE
+Rscript $REPORTSCRIPT $1'/outputs' 'median' 'NPAG' TRUE
+echo "results are in"  $1'/outputs/NPAGreport.html' ; fi
+mv npscript $1"/etc"
---
+++ pmetrics.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+#2020-02-14 Sabryr
+#Two argments mycycles and myindpts
+mycycles=100 #9997
+myindpts=108 #108
+
+if [ "$#" -eq 4 ]
+then
+	mycycles=$1 #9997
+	myindpts=$2 #108
+	model_file=$3  #$INPUT_DIR"/dataset.csv" 
+	dataset_file=$4 #$INPUT_DIR"/model.txt"
+	model_file_nm=$(echo $model_file | awk -F "/" '{print $NF}')
+	echo $model_file
+	echo $dataset_file
+	
+	#echo ${#model_file_nm}
+	#if [ ${#model_file_nm} -ge 8 ]
+	#then
+	#	echo "model_file name length should not exceed 8, your file name  "	
+	#	echo model_file | awk -F "/" '{print $NF}'
+	#	exit 1
+	#fi
+else
+	echo "Need four arguments"
+	echo "1. mycycles"
+	echo "2. myindpts"
+	echo "3. Full path to dataset"
+	echo "4. Full path to model file"
+	exit 1
+fi
+
+echo "Processing"
+#exit 1
+
+
+LOC=$PWD
+OUT_DIR=$LOC"/out_"$(date +"%Y%m%d_%H%M%S")
+LOG=$LOC"/out_"$(date +"%Y%m%d_%H%M%S")".log"
+echo "LOG "$LOG
+touch $LOG
+FCONF=$LOC"/FortConfig.txt"
+RSCRIPT=$LOC"/pmetrics.rscript"
+export R_LIBS=$LOC"/R"
+mkdir $R_LIBS &> /dev/null
+NPSCRIPT=$LOC"/npcsript_hpc.sh"
+REPORTSCRIPT=$LOC"/pmreport.rscript"
+
+touch $R_LIBS"/test"
+if [ $? -eq 0 ]
+then
+	echo "R packages will be installed to "$R_LIBS
+	sleep 2
+else
+	echo "$R_LIBS not found"
+fi
+
+echo $LOC
+echo $LOG
+echo $OUT_DIR
+echo $FCONF
+
+echo $(date) > $LOG
+
+#ls Pmetrics_*.tar.gz &> /dev/null
+
+#if [ $? -eq 0 ]
+#then
+#  Pmetricstar=$(ls $PWD/Pmetrics_*.tar.gz)
+#  echo "found "$Pmetricstar
+#else
+#  echo "Pmetrics tar not found in current directory "$PWD
+#  echo "you may donwload Pmetrics_1.5.1.tar.gz using the link"
+#  echo " wget http://www.lapk.org/software/Pmetrics/Repos/_gsdata_/_saved_/src/contrib/Pmetrics_1.5.1.tar.gz" 
+#fi
+
+mkdir -p $HOME/.config/Pmetrics &> /dev/null
+cp $FCONF $HOME/.config/Pmetrics/
+
+echo "------------------------------"
+echo $RSCRIPT $Pmetricstar $LOC $model_file $dataset_file $mycycles $myindpts
+$RSCRIPT $LOC $model_file $dataset_file $mycycles $myindpts &>> $LOG
+
+echo $RSCRIPT " -- done"
+mv 1 $OUT_DIR
+cd $OUT_DIR
+
+echo "directory rename from 1 to $OUT_DIR -- done "
+
+echo $NPSCRIPT $OUT_DIR $R_LIBS $REPORTSCRIPT 
+$NPSCRIPT $OUT_DIR $R_LIBS $REPORTSCRIPT
+echo "Done, output folder is $OUT_DIR"
+stty sane
+
